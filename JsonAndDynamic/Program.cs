16cd6b096885f8a2f2243c51ddecf8d181999d38
Billy Mumby <billy.mumby@outlook.com>
2021-11-14 15:42:58 +0000

    Have to deserialize a dynamic twice to a strongly typed object.

---
+++ JsonAndDynamic/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+var john = new Person("123", "John");
+
+var settings = new JsonSerializerOptions
+{
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    WriteIndented = true,
+    PropertyNameCaseInsensitive = true
+};
+
+var json = JsonSerializer.Serialize(john, settings);
+
+Console.WriteLine(json);
+
+var dynamicPerson = JsonSerializer.Deserialize<dynamic>(json, settings);
+
+var newPerson = JsonSerializer.Deserialize<Person>(dynamicPerson, settings);
+
+Console.WriteLine(newPerson);
+record Person(string Id, string Name);
